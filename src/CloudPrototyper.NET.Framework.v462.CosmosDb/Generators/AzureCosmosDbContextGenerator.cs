eee1400ad28c6da634819f54ab758fb1cd64055e
Petr Vitovsky <vitovsky.petr@gmail.com>
2021-07-24 13:11:19 +0200

    cosmosdb init

---
+++ src/CloudPrototyper.NET.Framework.v462.CosmosDb/Generators/AzureCosmosDbContextGenerator.cs
@@ -0,0 +1,52 @@
+﻿using CloudPrototyper.NET.Framework.v462.Common.Generators.DataLayerGenerators.DataFactories;
+using CloudPrototyper.NET.Framework.v462.Common.Generators.DataLayerGenerators.Entities;
+using CloudPrototyper.NET.Framework.v462.CosmosDb.Model;
+using CloudPrototyper.NET.Framework.v462.CosmosDb.Templates;
+using CloudPrototyper.NET.Interface.Generation;
+using CloudPrototyper.NET.Interface.Generation.Informations;
+using CloudPrototyper.NET.Interface.Prototyper;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace CloudPrototyper.NET.Framework.v462.CosmosDb.Generators
+{
+    public class AzureCosmosDbContextGenerator : Modeled<AzureCosmosDbContainer>, IEntityStorage
+    {
+        public List<EntityGenerator> Entities { get; set; }
+        public StorageInterfaceGenerator StorageInterface { get; set; }
+        public QueryGenerator Query { get; set; }
+        public DataGeneratorGenerator DataGenerator { get; set; }
+
+        public override List<PackageConfigInfo> GetNugetPackages() => new List<PackageConfigInfo>
+        {
+
+            new PackageConfigInfo(new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>(
+                    "",
+                    @"")
+            }, "System.Linq.Dynamic.Core", "1.2.11", "net462"),
+
+            new PackageConfigInfo(new List<Tuple<string, string>>
+            {
+                new Tuple<string, string>(
+                    "",
+                    @"")
+            }, "Microsoft.Azure.Cosmos", "3.20.1", "net462")
+
+        };
+
+        public AzureCosmosDbContextGenerator(string projectName, AzureCosmosDbContainer modelParameters, IList<EntityGenerator> entities,
+    StorageInterfaceGenerator storageInterface, QueryGenerator query, DataGeneratorGenerator dataGenerator)
+    : base(
+        projectName, modelParameters.Name, modelParameters.Name + "Context", typeof(AzureCosmosDbContextTemplate),
+        modelParameters, modelParameters.Name)
+        {
+            Query = query;
+            Entities = entities.Where(x => modelParameters.EntitySets.Select(y => y.EntityName).ToList().Contains(x.Name)).ToList();
+            StorageInterface = storageInterface;
+            DataGenerator = dataGenerator;
+        }
+    }
+}
---
+++ src/CloudPrototyper.NET.Framework.v462.CosmosDb/Model/AzureCosmosDbContainer.cs
@@ -0,0 +1,23 @@
+﻿using CloudPrototyper.Model.Resources.Storage;
+using Newtonsoft.Json;
+
+namespace CloudPrototyper.NET.Framework.v462.CosmosDb.Model
+{
+    public class AzureCosmosDbContainer : KeyValueDatabase
+    {
+        /// <summary>
+        /// Throughput type, "manual" or "autoscale"
+        /// </summary>
+        public string ThroughputType { get; set; }
+        /// <summary>
+        /// Number of RU/s for manual throughput, max RU/s for autoscale
+        /// </summary>
+        public int RUs { get; set; }
+        /// <summary>
+        /// Name of the container database
+        /// </summary>
+        public string DatabaseName { get; set; } = "";
+        [JsonIgnore]
+        public string ConnectionString { get; set; } = "";
+    }
+}
