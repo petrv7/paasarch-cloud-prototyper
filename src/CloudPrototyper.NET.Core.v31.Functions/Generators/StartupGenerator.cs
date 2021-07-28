4454b47f751beb927a044fa28643e2aaf809f2a1
Petr Vitovsky <vitovsky.petr@gmail.com>
2021-07-28 10:30:23 +0200

    removed message bus handler from startup

--- src/CloudPrototyper.NET.Core.v31.Functions/Generators/StartupGenerator.cs
+++ src/CloudPrototyper.NET.Core.v31.Functions/Generators/StartupGenerator.cs
@@ -9,14 +9,12 @@ namespace CloudPrototyper.NET.Core.v31.Functions.Generators
         public ActionBaseGenerator ActionBase { get; set; }
         public StorageInterfaceGenerator StorageInterface { get; set; }
         public MessageBusInterfaceGenerator MessageBusInterface { get; set; }
-        public MessageBusHandlerInterfaceGenerator MessageBusHandlerInterface { get; set; }
         public OperationInterfaceGenerator OperationInterface { get; set; }        
 
-        public StartupGenerator(string projectName, StorageInterfaceGenerator storageInterface, MessageBusInterfaceGenerator messageBusInterface, MessageBusHandlerInterfaceGenerator messageBusHandlerInterface, OperationInterfaceGenerator operationInterface, ActionBaseGenerator actionBase, bool canInitialize = true) : base(projectName, "Utils", "Startup", typeof(StartupTemplate), canInitialize)
+        public StartupGenerator(string projectName, StorageInterfaceGenerator storageInterface, MessageBusInterfaceGenerator messageBusInterface, OperationInterfaceGenerator operationInterface, ActionBaseGenerator actionBase, bool canInitialize = true) : base(projectName, "Utils", "Startup", typeof(StartupTemplate), canInitialize)
         {
             StorageInterface = storageInterface;
             MessageBusInterface = messageBusInterface;
-            MessageBusHandlerInterface = messageBusHandlerInterface;
             OperationInterface = operationInterface;
             ActionBase = actionBase;
         }
