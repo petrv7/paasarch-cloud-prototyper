145dbdbc20b4d8050ede57b5f4e3e00788ff99ce
Petr Vitovsky <vitovsky.petr@gmail.com>
2021-07-28 11:31:04 +0200

    updated nuget versions

--- src/CloudPrototyper.NET.Framework.v462.Common/Factories/NugetFactory.cs
+++ src/CloudPrototyper.NET.Framework.v462.Common/Factories/NugetFactory.cs
@@ -80,14 +80,12 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
                     @"..\packages\Microsoft.Data.Services.Client.5.6.4\lib\net40\Microsoft.Data.Services.Client.dll")
             }, "Microsoft.Data.Services.Client", "5.6.4", "net462"),
 
-
-
             new PackageConfigInfo(new List<Tuple<string, string>>
             {
                 new Tuple<string, string>("Newtonsoft.Json, Version=10.0.0.0, Culture=neutral, " +
                                           "PublicKeyToken=30ad4fe6b2a6aeed, processorArchitecture=MSIL",
                     @"..\packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll")
-            }, "Newtonsoft.Json", "10.0.3", "net462"),
+            }, "Newtonsoft.Json", "11.0.2", "net462"),
 
 
             new PackageConfigInfo(new List<Tuple<string, string>>
@@ -104,7 +102,7 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
                 new Tuple<string, string>(
                     "Microsoft.WindowsAzure.Storage, Version=7.2.1.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
                     @"..\packages\WindowsAzure.Storage.7.2.1\lib\net40\Microsoft.WindowsAzure.Storage.dll")
-            }, "WindowsAzure.Storage", "7.2.1", "net462"),
+            }, "WindowsAzure.Storage", "9.3.1", "net462"),
 
         };
 
--- src/CloudPrototyper.NET.Framework.v462.Common/Generators/DataLayerGenerators/Entities/EntityGenerator.cs
+++ src/CloudPrototyper.NET.Framework.v462.Common/Generators/DataLayerGenerators/Entities/EntityGenerator.cs
@@ -64,7 +64,7 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Generators.DataLayerGenerato
                 new Tuple<string, string>("Newtonsoft.Json, Version=10.0.0.0, Culture=neutral, " +
                                           "PublicKeyToken=30ad4fe6b2a6aeed, processorArchitecture=MSIL",
                     @"..\packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll")
-            }, "Newtonsoft.Json", "10.0.3", "net462"),
+            }, "Newtonsoft.Json", "11.0.2", "net462"),
 
 
             new PackageConfigInfo(new List<Tuple<string, string>>
@@ -81,7 +81,7 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Generators.DataLayerGenerato
                 new Tuple<string, string>(
                     "Microsoft.WindowsAzure.Storage, Version=7.2.1.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
                     @"..\packages\WindowsAzure.Storage.7.2.1\lib\net40\Microsoft.WindowsAzure.Storage.dll")
-            }, "WindowsAzure.Storage", "7.2.1", "net462"),
+            }, "WindowsAzure.Storage", "9.3.1", "net462"),
 
         };
 
