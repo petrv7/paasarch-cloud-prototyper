a21fb419d07e19f95341630232a95623574d399d
Petr Vitovsky <vitovsky.petr@gmail.com>
2021-08-04 13:35:13 +0200

    removed unused code

--- src/CloudPrototyper.NET.Framework.v462.Common/Factories/NugetFactory.cs
+++ src/CloudPrototyper.NET.Framework.v462.Common/Factories/NugetFactory.cs
@@ -29,7 +29,6 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
         /// <returns>Package info.</returns>
         public static List<PackageConfigInfo> MakeEntityFrameworkNuget() => new List<PackageConfigInfo>
         {
-
             new PackageConfigInfo(new List<Tuple<string, string>>
             {
                 new Tuple<string, string>(
@@ -39,7 +38,6 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
                     "EntityFramework.SqlServer, Version=6.0.0.0, Culture = neutral, PublicKeyToken = b77a5c561934e089, processorArchitecture = MSIL",
                     @"..\packages\EntityFramework.6.4.4\lib\net45\EntityFramework.SqlServer.dll")
             }, "EntityFramework", "6.4.4", "net462")
-
         };
 
         /// <summary>
@@ -48,62 +46,12 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
         /// <returns>Package info.</returns>
         public static List<PackageConfigInfo> MakeAzureTableStorage() => new List<PackageConfigInfo>
         {
-
-            /*new PackageConfigInfo(new List<Tuple<string, string>>
-            {
-                new Tuple<string, string>(
-                    "Microsoft.Azure.KeyVault.Core, Version=1.0.0.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
-                    @"..\packages\Microsoft.Azure.KeyVault.Core.1.0.0\lib\net40\Microsoft.Azure.KeyVault.Core.dll")
-            }, "Microsoft.Azure.KeyVault.Core", "1.0.0", "net462"),
-
-            new PackageConfigInfo(new List<Tuple<string, string>>
-            {
-                new Tuple<string, string>(
-                    "Microsoft.Data.Edm, Version=5.6.4.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
-                    @"..\packages\Microsoft.Data.Edm.5.6.4\lib\net40\Microsoft.Data.Edm.dll")
-            }, "Microsoft.Data.Edm", "5.6.4", "net462"),
-
-
-            new PackageConfigInfo(new List<Tuple<string, string>>
-            {
-                new Tuple<string, string>(
-                    "Microsoft.Data.OData, Version=5.6.4.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
-                    @"..\packages\Microsoft.Data.OData.5.6.4\lib\net40\Microsoft.Data.OData.dll")
-            }, "Microsoft.Data.OData", "5.6.4", "net462"),
-
-
-            new PackageConfigInfo(new List<Tuple<string, string>>
-            {
-                new Tuple<string, string>(
-                    "Microsoft.Data.Services.Client, Version=5.6.4.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL"
-                    ,
-                    @"..\packages\Microsoft.Data.Services.Client.5.6.4\lib\net40\Microsoft.Data.Services.Client.dll")
-            }, "Microsoft.Data.Services.Client", "5.6.4", "net462"),
-
-            new PackageConfigInfo(new List<Tuple<string, string>>
-            {
-                new Tuple<string, string>("Newtonsoft.Json, Version=10.0.0.0, Culture=neutral, " +
-                                          "PublicKeyToken=30ad4fe6b2a6aeed, processorArchitecture=MSIL",
-                    @"..\packages\Newtonsoft.Json.10.0.3\lib\net45\Newtonsoft.Json.dll")
-            }, "Newtonsoft.Json", "11.0.2", "net462"),
-
-
-            new PackageConfigInfo(new List<Tuple<string, string>>
-            {
-                new Tuple<string, string>(
-                    "System.Spatial, Version=5.6.4.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
-                    @"..\packages\System.Spatial.5.6.4\lib\net40\System.Spatial.dll")
-            }, "System.Spatial", "5.6.4", "net462"),*/
-
-
-
             new PackageConfigInfo(new List<Tuple<string, string>>
             {
                 new Tuple<string, string>(
                     "Microsoft.WindowsAzure.Storage, Version=7.2.1.0, Culture=neutral, PublicKeyToken=31bf3856ad364e35, processorArchitecture=MSIL",
                     @"..\packages\WindowsAzure.Storage.7.2.1\lib\net40\Microsoft.WindowsAzure.Storage.dll")
-            }, "WindowsAzure.Storage", "9.3.3", "net462"),
-
+            }, "WindowsAzure.Storage", "9.3.3", "net462")
         };
 
 
@@ -113,14 +61,12 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
         /// <returns>Package info.</returns>
         public static List<PackageConfigInfo> MakeCastleWidsorNuget() => new List<PackageConfigInfo>
         {
-
             new PackageConfigInfo(new List<Tuple<string, string>>
             {
                 new Tuple<string, string>(
                     "Castle.Windsor, Version=3.4.0.0, Culture=neutral, PublicKeyToken=407dd0808d44fbdc, processorArchitecture=MSIL",
                     @"..\packages\Castle.Windsor.3.4.0\lib\net45\Castle.Windsor.dll")
             }, "Castle.Windsor", "3.4.0", "net462"),
-
 
             new PackageConfigInfo(
 
@@ -130,7 +76,6 @@ namespace CloudPrototyper.NET.Framework.v462.Common.Factories
                         "Castle.Core, Version=3.3.0.0, Culture=neutral, PublicKeyToken=407dd0808d44fbdc, processorArchitecture=MSIL",
                         @"..\packages\Castle.Core.3.3.0\lib\net45\Castle.Core.dll")
                 }, "Castle.Core", "3.3.0", "net462")
-
         };
     }
 }
