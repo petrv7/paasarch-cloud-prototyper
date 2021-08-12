f0bb34d1b510ab37b1181ec1e20f3bf054cbe9e0
Petr Vitovsky <vitovsky.petr@gmail.com>
2021-08-12 16:07:54 +0200

    added simulate computation operation

---
+++ src/CloudPrototyper.NET.Framework.v462.Computing/Generators/SimulateComputationGenerator.cs
@@ -0,0 +1,17 @@
+﻿using CloudPrototyper.NET.Framework.v462.Computing.Models;
+using CloudPrototyper.NET.Framework.v462.Computing.Templates;
+using CloudPrototyper.NET.Interface.Generation;
+using CloudPrototyper.NET.Interface.Prototyper;
+
+namespace CloudPrototyper.NET.Framework.v462.Computing.Generators
+{
+    public class SimulateComputationGenerator : Modeled<SimulateComputation>, IOperation
+    {
+        public OperationInterfaceGenerator OperationInterface { get; set; }
+
+        public SimulateComputationGenerator(string projectName, OperationInterfaceGenerator operationInterface, SimulateComputation modelParameters, bool canInitialize = true) : base(projectName, "Operations", modelParameters.Name, typeof(SimulateComputationTemplate), modelParameters, modelParameters.Name, canInitialize)
+        {
+            OperationInterface = operationInterface;
+        }
+    }
+}
